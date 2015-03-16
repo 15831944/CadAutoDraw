65d88913c8ef31877cba4cbb4fe4401fe98b7fe2
猜猜看 <louis664724128@hotmail.com>
2015-03-17 00:47:08 +0800

    创建项目

---
+++ AutoDrawDWG/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace AutoDrawDWG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.T_ProjectName = new System.Windows.Forms.TextBox();
+            this.B_Valide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // T_ProjectName
+            // 
+            this.T_ProjectName.Location = new System.Drawing.Point(12, 12);
+            this.T_ProjectName.Name = "T_ProjectName";
+            this.T_ProjectName.Size = new System.Drawing.Size(100, 21);
+            this.T_ProjectName.TabIndex = 0;
+            this.T_ProjectName.TextChanged += new System.EventHandler(this.T_ProjectName_TextChanged);
+            // 
+            // B_Valide
+            // 
+            this.B_Valide.Location = new System.Drawing.Point(118, 12);
+            this.B_Valide.Name = "B_Valide";
+            this.B_Valide.Size = new System.Drawing.Size(75, 23);
+            this.B_Valide.TabIndex = 1;
+            this.B_Valide.Text = "创建或读取";
+            this.B_Valide.UseVisualStyleBackColor = true;
+            this.B_Valide.Click += new System.EventHandler(this.B_Valide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 188);
+            this.Controls.Add(this.B_Valide);
+            this.Controls.Add(this.T_ProjectName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox T_ProjectName;
+        private System.Windows.Forms.Button B_Valide;
+    }
+}
+
---
+++ AutoDrawDWG/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoDrawDWG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string ProjectName;
+
+        private void B_Valide_Click(object sender, EventArgs e)
+        {
+            if (T_ProjectName.Text == "")
+            {
+                using (OpenFileDialog fileDialog = new OpenFileDialog())
+                {
+                    fileDialog.Multiselect = false;
+                    fileDialog.Title = "请选择文件.";
+                    fileDialog.Filter = "cad|*.dwg|所有文件(*.*)|*.*";
+                    if (fileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string NameAndExtention = fileDialog.FileName;
+
+                        string[] filePathAndExtentions = NameAndExtention.Split(new[] { "\\" }, StringSplitOptions.None);
+                        string fileNameAndExtention = filePathAndExtentions[filePathAndExtentions.Length - 1];
+
+                        string[] fileNameAndExtentions = fileNameAndExtention.Split(new[] { "." }, StringSplitOptions.None);
+                        string fileName = fileNameAndExtentions[0];
+                        T_ProjectName.Text = fileName;
+                        ProjectName = fileName;
+                    }
+
+                }
+                this.Text = "绘制" + ProjectName + "线";
+                //groupBox1.Text = ProjectName;
+
+                this.Size = new Size(300, 300);
+                //添加第一层Groupbox
+                GroupBox gb = new GroupBox();
+                gb.Name = "MainGroueBox";
+                gb.Location = new Point(12, 41);
+                gb.Size = new Size(this.Size.Width / 2 - 20, this.Size.Height - 92);
+                gb.Text = "设置站点";
+                this.Controls.Add(gb);
+
+                ///在第一层GroupBox中添加按钮用来设置项目中站点
+                /// 添加textBox和按钮
+                /// 
+                //textBox
+                TextBox T_AddStation = new TextBox();
+                T_AddStation.Name = "T_AddStation";
+                T_AddStation.Text = "添加站点";
+                T_AddStation.Location = new Point(8, 24);
+                T_AddStation.Size = new Size(gb.Size.Width - 16, 24);
+
+                //按钮
+                Button B_AddStation = new Button();
+                B_AddStation.Name = "B_AddStation";
+                B_AddStation.Text = "添加站点";
+                B_AddStation.Location = new Point(8, 54);
+                B_AddStation.Size = new Size(gb.Size.Width - 16, 23);
+
+                //listBox
+                ListBox L_AddStation = new ListBox();
+                L_AddStation.Name = "B_AddStation";
+                L_AddStation.Location = new Point(8, 83);
+                L_AddStation.Size = new Size(gb.Size.Width - 16, gb.Size.Height - 83 - 10);
+
+                gb.Controls.Add(T_AddStation);  //添加textBox
+                gb.Controls.Add(B_AddStation);  //添加按钮
+                gb.Controls.Add(L_AddStation);  //添加listBox
+                //在第一层GroupBox中添加tableLayoutPannel
+            }
+        }
+
+        private void T_ProjectName_TextChanged(object sender, EventArgs e)
+        {
+            if (T_ProjectName.Text != ProjectName)
+            {
+                ProjectName = T_ProjectName.Text;
+                this.Text = "绘制" + ProjectName + "线";
+                //groupBox1.Text = ProjectName;
+            }
+        }
+
+
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is GroupBox)
+                {
+                    c.Size = new Size(c.Size.Width, this.Size.Height - 92);
+                    foreach(Control subC in c.Controls)
+                    {
+                        if(subC is ListBox)
+                        {
+                            subC.Size = new Size(c.Size.Width - 12, c.Size.Height - 83 - 10);
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(223, 88);
+        }
+    }
+}
